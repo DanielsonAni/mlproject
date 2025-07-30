470a777126cd46309a0aacf9fe4d9e766a61f26a
DanielsonAni <anidaniel491@gmail.com>
2025-07-30 09:39:17 +0100

    New Commits

--- src/logger.py
+++ src/logger.py
@@ -2,12 +2,9 @@ import logging
 import os
 from datetime import datetime
 
-
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 os.makedirs(logs_path,exist_ok=True)
-
-LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
@@ -18,3 +15,4 @@ logging.basicConfig(
 
 
 )
+
